ef3fb98505c6a041e79fb53ca856516c540a5d1e
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-28 16:22:29 +0000

    num and matrhs

---
+++ 01_basics/)5_numOrMath.js
@@ -0,0 +1,15 @@
+const score = 400;
+console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+const num = 12.449904
+// console.log(num.toPrecision(3));
+const newOne = 10000000000000
+// console.log(newOne.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++Maths++++++++++++//
+
+console.log(Math)
+console.log(Math.abs(-4));
