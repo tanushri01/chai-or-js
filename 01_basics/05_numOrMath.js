13c9e0c65bb1c64fcf38bdfd6dd7c4e2d39869f3
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-28 16:43:11 +0000

    math and num

---
+++ 01_basics/05_numOrMath.js
@@ -0,0 +1,29 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+const num = 12.449904
+// console.log(num.toPrecision(3));
+const newOne = 10000000000000
+// console.log(newOne.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++Maths++++++++++++//
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.36))
+// console.log(Math.ceil(4.3));
+// const array = [1,2,4,6,7,88,999,8,9,9,9,9,]
+// console.log(Math.min(...array))
+// console.log(Math.max(...array))
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
