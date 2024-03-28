1582376c47119eb98c36c86beee16b760741f828
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-28 12:09:09 +0000

    string

---
+++ 01_basics/04_string.js
@@ -0,0 +1,11 @@
+let name = new String("tanushree")
+let myAge = 24;
+console.log(`hiii, my name is ${name.toUpperCase()} and my age is ${myAge}`)
+// console.log(name)
+// console.log(name.indexOf('e'))
+
+let newName = name.substring(0, 5);
+// console.log(newName)
+// console.log(name)
+let anotherName = name.slice(-8, 5);
+// console.log(anotherName)
