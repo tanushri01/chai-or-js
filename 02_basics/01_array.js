35e66c929b24f474d7d7cbb7418154ecb7a00404
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-28 18:05:34 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,19 @@
+// array 
+
+// const myArr = [0,1,2,3,4,5, ture, "Tanu"]
+const myArr = [0,1,2,3,4,5]
+console.log(myArr)
+const myHeros = ["shaktiman", "jadu"];
+const myArr2 = new Array(1,2,3,4)
+
+
+// push 
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+myArr.unshift(10)
+myArr.shift()
+
+
+console.log(myArr.includes(7))
+console.log(myArr.indexOf(7))
