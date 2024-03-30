43f0e6c5e7b10879e452c3f6e57b649831a39539
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-30 13:47:43 +0000

    scope

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,26 +1,42 @@
-const name = "tanu"
+// const name = "tanu"
 
-function addMe(num1, num2)
+// function addMe(num1, num2)
+// {
+//   console.log(num1 + num2, name )
+// }
+
+// addMe(1,2)
+
+// function addMe_2(num1, num2)
+// {
+//   return num1 + num2
+// }
+// //save in some another variable provide return
+// // function callin needs console
+// const newAdd = addMe_2(1,2)
+// console.log(newAdd)
+
+
+// function loginUserMessage(username)
+// {
+//   return `${username} yeah just logged in!`
+// }
+// const result =  loginUserMessage("tanu");
+
+// console.log(result)
+
+function myname()
 {
-  console.log(num1 + num2, name )
+  const name = "tanu";
+  function mySurname()
+  {
+   const surname = "soni"; 
+    console.log(surname)
+    console.log(name)
+  }
+  mySurname()
+    // console.log(surname)
+  
+    console.log(name)
 }
-
-addMe(1,2)
-
-function addMe_2(num1, num2)
-{
-  return num1 + num2
-}
-//save in some another variable provide return
-// function callin needs console
-const newAdd = addMe_2(1,2)
-console.log(newAdd)
-
-
-function loginUserMessage(username)
-{
-  return `${username} yeah just logged in!`
-}
-const result =  loginUserMessage("tanu");
-
-console.log(result)
+myname()
