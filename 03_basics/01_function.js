745ba838ca8d052b449dfc68de887667f332b91d
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-30 12:44:39 +0000

    functions

---
+++ 03_basics/01_function.js
@@ -0,0 +1,26 @@
+const name = "tanu"
+
+function addMe(num1, num2)
+{
+  console.log(num1 + num2, name )
+}
+
+addMe(1,2)
+
+function addMe_2(num1, num2)
+{
+  return num1 + num2
+}
+//save in some another variable provide return
+// function callin needs console
+const newAdd = addMe_2(1,2)
+console.log(newAdd)
+
+
+function loginUserMessage(username)
+{
+  return `${username} yeah just logged in!`
+}
+const result =  loginUserMessage("tanu");
+
+console.log(result)
