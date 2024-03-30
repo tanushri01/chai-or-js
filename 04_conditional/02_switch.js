d412a31d8184909b74de9c64ad18833edb654fd7
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-03-30 23:17:04 +0000

    swtch

---
+++ 04_conditional/02_switch.js
@@ -0,0 +1,24 @@
+const month = 3;
+
+switch(month)
+{
+    case 1 :
+        console.log(january);
+        break;
+    case 1 :
+        console.log(january);
+        break;
+    case 2 :
+        console.log('fabruary');
+        break;
+    case 3 :
+        console.log('march');
+        break;
+    case 4 :
+        console.log('april');
+        break;
+default :
+console.log("not match any case");
+break;
+    
+}
