bb3cfb36f6e0e2c3db758191aafe4df36277a316
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-04-01 13:34:46 +0000

    for loop

---
+++ 05_loops/02_while.js
@@ -0,0 +1,24 @@
+let value = 1;
+while(value< 10)
+{
+    value ++
+console.log(value);
+
+} 
+
+let myArray = ['1', '2','3','4','5']
+let value1 = 0;
+while(value1< myArray.length)
+{
+    console.log(`value of my array is ${myArray[value1]}`);
+    value1 ++
+} 
+
+let score = 1;
+
+do {
+    console.log(`value of my score is ${score}`);
+    score++;
+} while (score <= 10);
+
+
