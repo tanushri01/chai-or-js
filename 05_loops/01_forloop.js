3175da1e17310a3948f6aac89233c2b3f06bb18d
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-04-01 13:17:10 +0000

    for loop

---
+++ 05_loops/01_forloop.js
@@ -0,0 +1,27 @@
+// // for(let i = 0; i <=10; i++)
+// //   {
+// //     let element = i
+// //     if(element == 5)
+// //       {
+// //         console.log(element,"i am printing 5")
+// //       }
+// //     console.log(element)
+// //   }
+
+
+// for( let  i = 1; i<=10; i++)
+//   {
+//     console.log(`value of outerloop is ${i}`)
+//     for(let j = 1; j <= 10; j++)
+//       {
+//         console.log(i + ' * ' + j + ' = ' + i*j)
+//       }
+//   }
+
+const myrray = ["tanu", "kartikey", "sonal"];
+for(let i = 0; i < myrray.length; i++)
+  {
+ const elemnt = myrray[i]
+ console.log(elemnt)
+  }
+
