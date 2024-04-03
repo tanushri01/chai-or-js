62e3747a4642b5e63d9d911da4be926e45dced7b
tanushri01 <99954261+tanushri01@users.noreply.github.com>
2024-04-03 21:08:25 +0000

    map filter and foreach

--- 05_loops/03_array_loops.js
+++ 05_loops/03_array_loops.js
@@ -29,3 +29,34 @@ for (const [key, value] of myobj) {
 }
 // not iteratable
 
+// method creates a new array by applying a function to each element of the original array. It returns a new array with the same length as the original array, where each element is the result of the applied function.
+let array = [1,2,3,4]
+const newArray =  array.map((item) =>{ return item + 1})
+console.log(newArray);
+console.log(array)
+
+
+// forEach perform every single data from the array and not returing a value 
+
+let arrayForEach = [1,2,3,4]
+let newArrayForEach = [];
+let newArray2ForEach = [];
+array.forEach((item) => {
+    newArrayForEach.push(item + 1);
+    newArray2ForEach.push(item * 5);
+});
+
+
+console.log(newArrayForEach);
+console.log(newArray2ForEach);
+
+// filter is used to select elements from an array based on a condition and return a new array with only the elements that satisfy the condition.
+// It takes a function as an argument which returns true or false based on the condition.
+// The result of the filter function is a new array containing only the elements for which the provided function returns true.
+// filter does not modify the original array.
+
+let arrayFilter = [1,2,3,4]
+const evenNumb = arrayFilter.filter((x)=>{
+  return (x % 2 === 0 ); 
+})
+console.log(evenNumb);
